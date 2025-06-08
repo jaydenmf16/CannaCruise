fde7fad2138784902b89c0b21a011a58059dbda4
CannaCruise <contactjaydenfields@icloud.com>
2025-06-08 10:51:56 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function bookRide() {
+  alert("Booking feature coming soon!");
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  alert("Thanks for reaching out! We will get back to you soon.");
+  event.target.reset();
+}
